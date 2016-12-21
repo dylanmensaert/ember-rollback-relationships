f81acb636b6386a3e92a4fb8da3f56b563cc9377
dylan.mensaert@gmail.com <dylan.mensaert@gmail.com>
2016-12-21 10:19:10 +0100

    Fixed issue.

--- app/initializers/reopen-model.js
+++ app/initializers/reopen-model.js
@@ -38,6 +38,9 @@ export default {
 
                 return changedRelationships;
             },
+            ready: function() {
+                this.resetOldRelationships();
+            },
             didCreate: function() {
                 this.resetOldRelationships();
             },
